2907e8de41c3fabcc3cfbaaa4e7f6e0a7762d9fd
Adhiraj Chaudhary <adhirajchaudhary@gmail.com>
2026-06-30 02:08:54 +0530

    Fix dotenv path

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '/app/.env' });
 // Only load .env file in local development
 const express = require('express');
 const twilio = require('twilio');
